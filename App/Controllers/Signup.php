a2c6484c01659aa85a2e6861ffb82dfb5fc3c6a0
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-16 20:50:24 +0100

    Added showing template in function

--- App/Controllers/Signup.php
+++ App/Controllers/Signup.php
@@ -2,9 +2,12 @@
 
 namespace App\Controllers;
 
+use Core\View;
+
 class Signup extends \Core\Controller
 {
     public function newAction()
     {
+        View::renderTemplate('Signup/new.html');
     }
 }
