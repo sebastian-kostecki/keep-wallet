c3cbff93ac7faefbc8e2b7d7c75fff1f7d105be7
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-16 08:34:57 +0100

    Added display page indexAction method

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -2,9 +2,12 @@
 
 namespace App\Controllers;
 
+use Core\View;
+
 class Home extends \Core\Controller
 {
     public function indexAction()
     {
+        View::renderTemplate('Home/index.html');
     }
 }
