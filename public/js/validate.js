a6a88d61d51e47bac9c79fcd41af5257ef470722
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-17 16:44:42 +0100

    Added validate function

---
+++ public/js/validate.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('#form').validate({
+        rules: {
+            name: 'required',
+            email: {
+                required: true,
+                email: true,
+            },
+            firstPassword: {
+                required: true,
+                minlength: 6,
+            }
+        }
+    });
+});
