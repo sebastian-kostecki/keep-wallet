53c089a51070e03769956a22e4d54e37c0ab69e2
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-18 08:50:54 +0100

    Created new method: validateNewUser in User model

--- App/Models/User.php
+++ App/Models/User.php
@@ -16,13 +16,17 @@ class User extends \Core\Model
     public function saveUser()
     {
         $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
-        
+
         $db = static::getDataBase();
         $newUserQuery = $db->prepare('INSERT INTO users VALUES (NULL, :name, :password, :email)');
         $newUserQuery->bindValue(':name', $this->name, PDO::PARAM_STR);
         $newUserQuery->bindValue(':password', $password_hash, PDO::PARAM_STR);
         $newUserQuery->bindValue(':email', $this->email, PDO::PARAM_STR);
-        
+
         return $newUserQuery->execute();
+    }
+
+    public function validateNewUser()
+    {
     }
 }
