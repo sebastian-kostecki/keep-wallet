9701c5adbf5ae881c3f08e9ea317b653100d8374
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-18 20:08:13 +0100

    Added function: validateEmail

--- App/Controllers/Account.php
+++ App/Controllers/Account.php
@@ -6,4 +6,11 @@ use App\Models\User;
 
 class Account extends \Core\Controller
 {
+    public function validateEmailAction()
+    {
+        $isValidEmail = !User::isEmailExists($_GET['email']);
+
+        header('Content-type: application/json');
+        echo json_encode($isValidEmail);
+    }
 }
