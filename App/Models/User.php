9d89eb77f911dc87da5a308575815b8b5909d6da
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-18 15:40:49 +0100

    Added validate in backend side

--- App/Models/User.php
+++ App/Models/User.php
@@ -17,15 +17,20 @@ class User extends \Core\Model
 
     public function saveUser()
     {
-        $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
+        $this->validateNewUser();
 
-        $db = static::getDataBase();
-        $newUserQuery = $db->prepare('INSERT INTO users VALUES (NULL, :name, :password, :email)');
-        $newUserQuery->bindValue(':name', $this->name, PDO::PARAM_STR);
-        $newUserQuery->bindValue(':password', $password_hash, PDO::PARAM_STR);
-        $newUserQuery->bindValue(':email', $this->email, PDO::PARAM_STR);
+        if (empty($this->errors)) {
+            $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
 
-        return $newUserQuery->execute();
+            $db = static::getDataBase();
+            $newUserQuery = $db->prepare('INSERT INTO users VALUES (NULL, :name, :password, :email)');
+            $newUserQuery->bindValue(':name', $this->name, PDO::PARAM_STR);
+            $newUserQuery->bindValue(':password', $password_hash, PDO::PARAM_STR);
+            $newUserQuery->bindValue(':email', $this->email, PDO::PARAM_STR);
+
+            return $newUserQuery->execute();
+        }
+        return false;
     }
 
     public function validateNewUser()
@@ -33,7 +38,8 @@ class User extends \Core\Model
         if ($this->name == '') {
             $this->errors[] = 'Wpisz imię';
         }
-        if (preg_match('/.*[\$&\+,:;=?[\]@#|{}\'<>.^*()%!-/]+.*/i', $this->name)) {
+
+        if (preg_match("/.*[$&+,:;=?[\]@#|{}'<>.^*()%!-\/]+.*/i", $this->name)) {
             $this->errors[] = 'Imię nie może zawierać znaków specjalnych';
         }
 
