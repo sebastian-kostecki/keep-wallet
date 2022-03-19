5c1a57f1ef0cf50f010faaf1874d76761629ab53
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-19 18:53:18 +0100

    Created login method in Authentication class

--- App/Authentication.php
+++ App/Authentication.php
@@ -4,4 +4,15 @@ namespace App;
 
 class Authentication
 {
+    public static function login($user, $rememberMe)
+    {
+        session_regenerate_id(true);
+        $_SESSION['userId'] = $user->id;
+
+        if ($rememberMe) {
+            if ($user->rememberLogin()) {
+                setcookie('remember_me', $user->remember_token, $user->expiry_timestamp, '/');
+            }
+        }
+    }
 }
