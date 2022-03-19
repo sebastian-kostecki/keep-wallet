99bd79f57160c2a5acabef6b62000a6cf6e8071d
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-19 20:57:47 +0100

    Created main menu after login

--- App/Controllers/Menu.php
+++ App/Controllers/Menu.php
@@ -2,6 +2,12 @@
 
 namespace App\Controllers;
 
+use Core\View;
+
 class Menu extends \Core\Controller
 {
+    public function showAction()
+    {
+        View::renderTemplate('Menu/menu.html');
+    }
 }
