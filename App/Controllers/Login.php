a9c686482049567b0e3bea3e5dddfa226f6af3a6
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-19 15:00:52 +0100

    Created method show in login controller

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -2,6 +2,12 @@
 
 namespace App\Controllers;
 
+use Core\View;
+
 class Login extends \Core\Controller
 {
+    public function showAction()
+    {
+        View::renderTemplate('Login/show.html');
+    }
 }
