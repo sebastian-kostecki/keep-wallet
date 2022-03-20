1ce04cf267e6cc1c2000369cc62285b81c012604
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-20 08:25:07 +0100

    Added addMessage and getMessage functions for flash messages

--- App/Flash.php
+++ App/Flash.php
@@ -4,4 +4,29 @@ namespace App;
 
 class Flash
 {
+    const SUCCESS = 'success';
+    const INFO = 'info';
+    const WARNING = 'warning';
+
+    public static function addMessage($message, $type = 'success')
+    {
+        if (!isset($_SESSION['flash_notifications'])) {
+            $_SESSION['flash_notifications'] = [];
+        }
+
+        $_SESSION['flash_notifications'][] = [
+            'body' => $message,
+            'type' => $type
+        ];
+    }
+
+    public static function getMessage()
+    {
+        if (isset($_SESSION['flash_notifications'])) {
+            $messages = $_SESSION['flash_notifications'];
+            unset($_SESSION['flash_notifications']);
+
+            return $messages;
+        }
+    }
 }
