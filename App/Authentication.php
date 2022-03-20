d6e9537ada9fe80f5d61afcd0332ec44e31b8325
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-20 11:56:01 +0100

    Added prevent login by cookie if token expired

--- App/Authentication.php
+++ App/Authentication.php
@@ -61,7 +61,7 @@ class Authentication
         if ($cookie) {
             $remembered_login = RememberedLogin::findByToken($cookie);
 
-            if ($remembered_login) {
+            if ($remembered_login && $remembered_login->hasExpired()) {
                 $user = $remembered_login->getUser();
                 static::login($user, false);
                 return $user;
