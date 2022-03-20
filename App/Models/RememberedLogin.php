65f75bfc0933dd8dc33fbc8ddf3dc9ea42d04e74
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-20 11:45:04 +0100

    Added finding user by token - cookie

--- App/Models/RememberedLogin.php
+++ App/Models/RememberedLogin.php
@@ -2,6 +2,22 @@
 
 namespace App\Models;
 
+use App\Token;
+use PDO;
+
 class RememberedLogin extends \Core\Model
 {
+    public static function findByToken($token)
+    {
+        $token = new Token($token);
+        $token_hash = $token->getHash();
+
+        $db = static::getDataBase();
+        $query = $db->prepare('SELECT * FROM remebered_logins WHERE token_hash = :token_hash');
+        $query->bindValue(':token_hash', $token_hash, PDO::PARAM_STR);
+        $query->execute();
+
+        $query->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+        return $query->fetch();
+    }
 }
