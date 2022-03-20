fc0efe786e676c2ab4fac31f5a902150e79afdf8
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-20 07:37:44 +0100

    Added before method to Authenticated controller

--- App/Controllers/Authenticated.php
+++ App/Controllers/Authenticated.php
@@ -4,4 +4,8 @@ namespace App\Controllers;
 
 class Authenticated extends \Core\Controller
 {
+    protected function before()
+    {
+        //tutaj akcja sprawdzania zalogowania użytkownika
+    }
 }
