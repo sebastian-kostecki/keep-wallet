59bee83dabf669c621b77a0614fbe3a9e647f4ee
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-20 13:59:58 +0100

    Created empty method processing request reset password

--- App/Controllers/Password.php
+++ App/Controllers/Password.php
@@ -3,11 +3,20 @@
 namespace App\Controllers;
 
 use Core\View;
+use App\Models\User;
 
 class Password extends \Core\Controller
 {
     public function forgotAction()
     {
         View::renderTemplate('Password/forgot.html');
+    }
+
+    public function requestResetAction()
+    {
+        //wysyłamy do modelu żądanie przetworzenia prośby
+        //tam będzie wygenerowany model
+
+        //wyświetlenie komunikatu o wysłaniu emaila
     }
 }
