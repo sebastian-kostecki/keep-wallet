86492a7acef13adc34b55c78c43c0c1bdda50a0a
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-20 13:44:50 +0100

    Added new page with reset password

--- App/Controllers/Password.php
+++ App/Controllers/Password.php
@@ -2,6 +2,12 @@
 
 namespace App\Controllers;
 
+use Core\View;
+
 class Password extends \Core\Controller
 {
+    public function forgotAction()
+    {
+        View::renderTemplate('Password/forgot.html');
+    }
 }
