057d555939b22f76acd9acc1f0d0e90ff1692fa7
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-21 22:11:22 +0100

    Created showing base page

--- App/Controllers/Income.php
+++ App/Controllers/Income.php
@@ -2,6 +2,16 @@
 
 namespace App\Controllers;
 
+use App\Models\User;
+use Core\View;
+
 class Income extends Authenticated
 {
+    public function addAction()
+    {
+        $user = User::findByID($_SESSION['userId']);
+        View::renderTemplate('Income/add.html', [
+            'user' => $user
+        ]);
+    }
 }
