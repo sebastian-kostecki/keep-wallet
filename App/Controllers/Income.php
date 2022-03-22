b87dfbce2831a960758feae40e33c4637445ef72
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-22 14:56:21 +0100

    Added categories do controller

--- App/Controllers/Income.php
+++ App/Controllers/Income.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Models\IncomeCategory;
 use App\Models\User;
 use Core\View;
 
@@ -10,9 +11,11 @@ class Income extends Authenticated
     public function newAction()
     {
         $user = User::findByID($_SESSION['userId']);
+        $userIncomeCategories = IncomeCategory::findCategories($user);
         //trzeba pobrać kategorie po userze
         View::renderTemplate('Income/new.html', [
-            'user' => $user
+            'user' => $user,
+            'incomeCategories' => $userIncomeCategories
         ]);
     }
 }
