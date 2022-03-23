d4f773fcb48cdf5c7bbad8e0229e3311f73f7be3
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-23 12:59:09 +0100

    Created empty model ExpenseCategory with constructor

---
+++ App/Models/ExpenseCategory.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+class ExpenseCategory extends \Core\Model
+{
+    public function __construct($data = [])
+    {
+        foreach ($data as $key => $value) {
+            $this->$key = $value;
+        }
+    }
+}
