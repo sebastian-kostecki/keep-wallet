945d8a92b2b5fff3d2859f09869a3ec834f498dd
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-23 13:02:24 +0100

    Got user expense categories to controller

--- App/Controllers/Expense.php
+++ App/Controllers/Expense.php
@@ -3,11 +3,13 @@
 namespace App\Controllers;
 
 use App\Models\User;
+use App\Models\ExpenseCategory;
 
 class Expense extends \Core\Controller
 {
     public function newAction()
     {
         $user = User::findByID($_SESSION['userId']);
+        $userExpenseCategories = ExpenseCategory::findCategories($user);
     }
 }
