fa97c6a9066ccbf01feec759a5e20ed8b5305361
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-23 14:39:12 +0100

    Added balance view

--- App/Controllers/Balance.php
+++ App/Controllers/Balance.php
@@ -2,9 +2,12 @@
 
 namespace App\Controllers;
 
+use Core\View;
+
 class Balance extends Authenticated
 {
     public function currentMonthAction()
     {
+        View::renderTemplate('Balance/currentMonth.html');
     }
 }
