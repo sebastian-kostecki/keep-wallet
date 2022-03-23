348536c412ee10995ec6206ab2a1871f2a326274
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-23 13:09:46 +0100

    Created base expense view based in income

--- App/Controllers/Expense.php
+++ App/Controllers/Expense.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use App\Models\User;
 use App\Models\ExpenseCategory;
 use App\Models\PaymentMethod;
+use Core\View;
 
 class Expense extends \Core\Controller
 {
@@ -13,5 +14,9 @@ class Expense extends \Core\Controller
         $user = User::findByID($_SESSION['userId']);
         $expenseCategories = ExpenseCategory::findCategories($user);
         $paymentMethods = PaymentMethod::findPaymentMethods($user);
+        View::renderTemplate('Expense/new.html', [
+            'user' => $user,
+            'expenseCategories' => $expenseCategories
+        ]);
     }
 }
