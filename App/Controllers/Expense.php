2a774312109954a5244f26faaa44ba55f7354278
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-23 12:57:51 +0100

    Created method new in Expense controller

--- App/Controllers/Expense.php
+++ App/Controllers/Expense.php
@@ -2,6 +2,12 @@
 
 namespace App\Controllers;
 
+use App\Models\User;
+
 class Expense extends \Core\Controller
 {
+    public function newAction()
+    {
+        $user = User::findByID($_SESSION['userId']);
+    }
 }
