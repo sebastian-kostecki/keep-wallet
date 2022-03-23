bb6a4c400014e7ffef2e8f7a350dd5c1c6e185a8
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-23 13:05:44 +0100

    Got payment methods from user

--- App/Controllers/Expense.php
+++ App/Controllers/Expense.php
@@ -4,12 +4,14 @@ namespace App\Controllers;
 
 use App\Models\User;
 use App\Models\ExpenseCategory;
+use App\Models\PaymentMethod;
 
 class Expense extends \Core\Controller
 {
     public function newAction()
     {
         $user = User::findByID($_SESSION['userId']);
-        $userExpenseCategories = ExpenseCategory::findCategories($user);
+        $expenseCategories = ExpenseCategory::findCategories($user);
+        $paymentMethods = PaymentMethod::findPaymentMethods($user);
     }
 }
