6778203a12fa597305f06e11bf3b5e8143ebd61d
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-24 14:21:26 +0100

    Added simple toggle

--- public/js/hideShowBalanceRow.js
+++ public/js/hideShowBalanceRow.js
@@ -1,2 +1,8 @@
 const rows = document.querySelectorAll('.category-row');
 const budgetItems = document.querySelectorAll('.budget-item-details');
+
+for (let row of rows) {
+    row.addEventListener('click', function () {
+        row.style.color = 'black'
+    })
+}
