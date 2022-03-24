38c0aafbe21ab2c89ec81bb1118ccf2fd7f3f616
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-24 14:44:06 +0100

    Added toggle row with category

--- public/js/hideShowBalanceRow.js
+++ public/js/hideShowBalanceRow.js
@@ -3,6 +3,29 @@ const budgetItems = document.querySelectorAll('.budget-item-details');
 
 for (let row of rows) {
     row.addEventListener('click', function () {
-        row.style.color = 'black'
+        //console.log(row.innerText);
+        //row.innerText -> obie wartości przedzielone /t
+        //czyli trzeba wyodrębnić części do tabulatora
+
+        let lineFromRow = row.innerText.split('\t');
+        //lineFromRow[0] -> to nazwa klasy ze spacjami
+
+        let words = lineFromRow[0].split(' ');
+
+        let className = "";
+        for (let word of words) {
+            className += "." + word;
+        }
+
+        const elementsToDisplay = document.querySelectorAll(className);
+
+        for (let elementToDisplay of elementsToDisplay) {
+            if (elementToDisplay.style.display === 'table-row') {
+                elementToDisplay.style.display = 'none';
+            } else {
+                elementToDisplay.style.display = 'table-row';
+            }
+        }
+
     })
 }
