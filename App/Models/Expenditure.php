15d358265e828ff24f904e0ed766ac37a8f067bf
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-24 12:33:34 +0100

    Added fetchExpenses to Expenditure model

--- App/Models/Expenditure.php
+++ App/Models/Expenditure.php
@@ -70,4 +70,58 @@ class Expenditure extends \Core\Model
             $this->errors[]  = 'Komentarz może zawierać maksymalnie 100 znaków';
         }
     }
+
+    protected static function fetchExpensesCategory($period)
+    {
+        $firstDay = substr($period, 0, 10);
+        $lastDay = substr($period, 11);
+
+        $sql = "SELECT expenses_category_assigned_to_users.name, SUM(expenses.amount) as total
+        FROM expenses INNER JOIN expenses_category_assigned_to_users 
+        WHERE expenses.user_id = :userId AND expenses.expense_category_assigned_to_user_id = expenses_category_assigned_to_users.id AND date_of_expense BETWEEN :firstDay AND :lastDay GROUP BY expenses.expense_category_assigned_to_user_id";
+
+        $db = static::getDataBase();
+        $query = $db->prepare($sql);
+        $query->bindValue(':userId', $_SESSION['userId'], PDO::PARAM_INT);
+        $query->bindValue(':firstDay', $firstDay, PDO::PARAM_STR);
+        $query->bindValue(':lastDay', $lastDay, PDO::PARAM_STR);
+        $query->execute();
+
+        $query->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+        return $query->fetchAll();
+    }
+
+    protected static function fetchAllExpenses($period)
+    {
+        $firstDay = substr($period, 0, 10);
+        $lastDay = substr($period, 11);
+
+        $sql = "SELECT expense_user.name, expenses.amount, expenses.date_of_expense, expenses.expense_comment, pm.name as payment_method 
+                FROM expenses_category_assigned_to_users as expense_user INNER JOIN expenses ON expenses.expense_category_assigned_to_user_id = expense_user.id INNER JOIN payment_methods_assigned_to_users as pm ON pm.id = expenses.payment_method_assigned_to_user_id 
+                WHERE expenses.user_id = :userId AND date_of_expense BETWEEN :firstDay AND :endDay ORDER BY expenses.date_of_expense";
+
+        $db = static::getDataBase();
+        $query = $db->prepare($sql);
+        $query->bindValue(':userId', $_SESSION['userId'], PDO::PARAM_INT);
+        $query->bindValue(':firstDay', $firstDay, PDO::PARAM_STR);
+        $query->bindValue(':lastDay', $lastDay, PDO::PARAM_STR);
+        $query->execute();
+
+        return $query->fetchAll();
+    }
+
+    public static function fetchExpenses($period)
+    {
+        $expensesByCategory = static::fetchExpensesCategory($period);
+        $expensesAll = static::fetchAllExpenses($period);
+
+        foreach ($expensesByCategory as $expenseByCategory) {
+            foreach ($expensesAll as $expense) {
+                if ($expenseByCategory->name == $expense['name']) {
+                    $expenseByCategory->incomes[] = $expense;
+                }
+            }
+        }
+        return $expensesByCategory;
+    }
 }
