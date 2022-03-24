bf68dabb17adfdc47073bc45ff88c333ce52962c
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-24 13:26:50 +0100

    Added display sum

---
+++ public/js/balanceCalculations.js
@@ -0,0 +1,19 @@
+const expensesAmount = document.querySelectorAll('.expense-amount');
+const incomesAmount = document.querySelectorAll('.income-amount');
+const incomesSum = document.querySelector('#incomes-sum');
+const expensesSum = document.querySelector('#expenses-sum');
+
+let sumIncomes = 0;
+let sumExpenses = 0;
+
+for (const income of incomesAmount) {
+    sumIncomes = sumIncomes + parseFloat(income.innerText);
+}
+
+incomesSum.innerText = sumIncomes.toFixed(2) + " zł";
+
+for (const expense of expensesAmount) {
+    sumExpenses += parseFloat(expense.innerText);
+}
+
+expensesSum.innerText = sumExpenses.toFixed(2) + " zł";
