45d37f9d5f7158fab1b020e7548d878309ec52f8
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-27 21:45:51 +0200

    Removed user from Income Controller

--- App/Models/IncomeCategory.php
+++ App/Models/IncomeCategory.php
@@ -6,14 +6,7 @@ use PDO;
 
 class IncomeCategory extends \Core\Model
 {
-    public function __construct($data = [])
-    {
-        foreach ($data as $key => $value) {
-            $this->$key = $value;
-        }
-    }
-
-    public static function findCategories($user)
+    public static function findCategories()
     {
         $sql = "SELECT * 
                 FROM incomes_category_assigned_to_users 
@@ -21,7 +14,7 @@ class IncomeCategory extends \Core\Model
 
         $db = static::getDataBase();
         $query = $db->prepare($sql);
-        $query->bindValue(':userId', $user->id, PDO::PARAM_INT);
+        $query->bindValue(':userId', $_SESSION['userId'], PDO::PARAM_INT);
         $query->execute();
 
         $query->setFetchMode(PDO::FETCH_CLASS, get_called_class());
