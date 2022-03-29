7edfeb4157faf25bc2f8e157693245650bd937a5
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-29 06:21:30 +0200

    Created show method in Settings Controller

--- App/Controllers/Settings.php
+++ App/Controllers/Settings.php
@@ -2,6 +2,12 @@
 
 namespace App\Controllers;
 
+use Core\View;
+
 class Settings extends Authenticated
 {
+    public function showAction()
+    {
+        View::renderTemplate('Settings/show.html');
+    }
 }
