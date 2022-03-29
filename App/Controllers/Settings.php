fb29a85185d39e8fb8d438c2921460b05d849a76
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-29 13:23:15 +0200

    Added getting expense categories to controller

--- App/Controllers/Settings.php
+++ App/Controllers/Settings.php
@@ -4,14 +4,17 @@ namespace App\Controllers;
 
 use Core\View;
 use App\Models\IncomeCategory;
+use App\Models\ExpenseCategory;
 
 class Settings extends Authenticated
 {
     public function showAction()
     {
         $userIncomeCategories = IncomeCategory::findCategories();
+        $expenseCategories = ExpenseCategory::findCategories();
         View::renderTemplate('Settings/show.html', [
-            'incomeCategories' => $userIncomeCategories
+            'incomeCategories' => $userIncomeCategories,
+            'expenseCategories' => $expenseCategories
         ]);
     }
 }
