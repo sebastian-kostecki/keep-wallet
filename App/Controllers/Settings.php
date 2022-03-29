ccada2dd4b19757510d817670cd64a1e5fc6ab7c
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-29 21:23:00 +0200

    Added icons to view in controller

--- App/Controllers/Settings.php
+++ App/Controllers/Settings.php
@@ -6,6 +6,7 @@ use Core\View;
 use App\Models\IncomeCategory;
 use App\Models\ExpenseCategory;
 use App\Models\PaymentMethod;
+use App\Models\Icon;
 
 class Settings extends Authenticated
 {
@@ -14,11 +15,13 @@ class Settings extends Authenticated
         $userIncomeCategories = IncomeCategory::findCategories();
         $expenseCategories = ExpenseCategory::findCategories();
         $paymentMethods = PaymentMethod::findPaymentMethods();
+        $icons = Icon::getIcons();
 
         View::renderTemplate('Settings/show.html', [
             'incomeCategories' => $userIncomeCategories,
             'expenseCategories' => $expenseCategories,
-            'paymentMethods' => $paymentMethods
+            'paymentMethods' => $paymentMethods,
+            'icons' => $icons
         ]);
     }
 }
