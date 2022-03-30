50c283195f10634039689368cf075ed52ffbc475
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-03-30 22:00:58 +0200

    Added working select icons

--- public/js/getIcon.js
+++ public/js/getIcon.js
@@ -1,34 +1,19 @@
 const buttonChosenIconInModal = document.querySelector('#button-chosen-icon-in-modal');
 const iconCheckButtons = document.querySelectorAll('.button-chosen-icon');
-const chosenIcon = document.querySelectorAll('.selected-icon-in-modal');
-const hiddenInputs = document.querySelectorAll('.chosen-icon-input-value');
 
 const myModal = new bootstrap.Modal(document.getElementById('choiceIncomeIcon'), {
     keyboard: false
 })
 
-
-
-
 for (let iconCheckButton of iconCheckButtons) {
     iconCheckButton.addEventListener('click', function () {
-
-        //działa tylko dla wybranego buttona
         buttonChosenIconInModal.addEventListener('click', function () {
-            let chosenIcon = document.querySelector('.selected-icon-in-modal:checked').value;
-            iconCheckButton.innerHTML = '<i class="' + chosenIcon + '">';
+            let chosenIcon = document.querySelector('.selected-icon-in-modal:checked');
+            iconCheckButton.innerHTML = '<i class="' + chosenIcon.value + '">';
+            iconCheckButton.nextElementSibling.value = chosenIcon.value;
             myModal.toggle();
         })
-
-        //let chosenIcon = document.querySelector('.selected-icon-in-modal:checked').value;
-
-        //iconCheckButton.innerHTML = '<i class="' + chosenIcon + '">';
-
-        //console.log(hiddenInput.value);
-        //myModal.toggle();
     })
-
-
 }
 
 
