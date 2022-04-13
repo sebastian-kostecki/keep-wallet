de9dc1c5c177e63521df656eab5708e6ab38c556
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-04-13 22:50:35 +0200

    Accomplished titles of settings

--- public/js/hideShowSettings.js
+++ public/js/hideShowSettings.js
@@ -1,16 +1,21 @@
-const settingList = document.querySelectorAll('.setting-list');
+const titlesOfSettings = document.querySelectorAll('.setting-list');
 const settingsForm = document.querySelectorAll('.settings-form');
 
-for (let legend of settingList) {
-    legend.addEventListener('click', function () {
-        let classForm = '.' + legend.id;
+for (let titles of titlesOfSettings) {
+    titles.addEventListener('click', function () {
+        let classForm = '.' + titles.id;
         let form = document.querySelector(classForm);
-
+        let markDown = titles.children[0].children[1].children[0];
+        let markUp = titles.children[0].children[1].children[1];
 
         if (form.style.display === 'block') {
             form.style.display = 'none';
+            markUp.style.display = 'none';
+            markDown.style.display = 'inline-block';
         } else {
             form.style.display = 'block';
+            markUp.style.display = 'inline-block';
+            markDown.style.display = 'none';
         }
     })
 };
