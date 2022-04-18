1bf8cbd1adeaf9cd7638b69fbc9baed274b078ee
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-04-18 23:55:10 +0200

    Added button modyfying budget items

--- public/js/changeBudgetItem.js
+++ public/js/changeBudgetItem.js
@@ -1,36 +1,44 @@
 const buttons = document.querySelectorAll('.button-change-budget-item');
+const buttonsChangingBudgetItems = document.querySelectorAll('.button-form-change-budget-item');
+
+const isButtonsNotShowing = () => {
+    for (let button of buttonsChangingBudgetItems) {
+        if (button.style.display == 'inline-block') {
+            return false;
+        }
+    }
+    return true;
+}
+
 
 for (let button of buttons) {
     button.addEventListener('click', function (e) {
-        let classNameForTableData = 'td.' + button.id;
-        let chosenTableDatas = document.querySelectorAll(classNameForTableData);
-        for (let td of chosenTableDatas) {
-            td.contentEditable = 'true';
+        if (isButtonsNotShowing()) {
+            let classNameForTableData = 'td.' + button.id;
+            let chosenTableDatas = document.querySelectorAll(classNameForTableData);
+            for (let td of chosenTableDatas) {
+                td.contentEditable = 'true';
+            }
+            button.hidden = true;
+            let buttonAbortingBudgetItem = button.nextElementSibling.lastElementChild;
+            let buttonChangingBudgetItem = button.nextElementSibling.lastElementChild.previousElementSibling;
+
+            buttonAbortingBudgetItem.style.display = 'inline-block';
+            buttonChangingBudgetItem.style.display = 'inline-block';
         }
-        button.hidden = true;
-        let buttonChangingBudgetItem = button.nextElementSibling.lastElementChild;
-        buttonChangingBudgetItem.style.display = 'inline-block';
     })
 
+
     button.parentElement.parentElement.addEventListener('mouseenter', function () {
-        button.innerHTML = '<i class="fas fa-pen ms-1 fs-6"></i>';
+        if (isButtonsNotShowing()) {
+            button.innerHTML = '<i class="fas fa-pen ms-1 fs-6"></i>';
+        }
     })
+
+
 
     button.parentElement.parentElement.addEventListener('mouseleave', function () {
         button.innerHTML = '';
     })
 }
 
-// const budgetItemInDetails = document.querySelectorAll('budget-item-details');
-// for (let item of budgetItemInDetails) {
-//     item.addEventListener('mouseenter', function () {
-//         button.style.display = 'block';
-//     })
-// }
-
-
-
-// var tds = document.getElementsByTagName('td');
-// for (var i = 0; i < tds.length; i++) {
-//     tds[i].contentEditable = 'true';
-// }
