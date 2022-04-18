42e93a61d12012f143f760b6630cedcc3d6b2ff2
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-04-18 23:08:08 +0200

    Added look for buttons

--- public/js/changeBudgetItem.js
+++ public/js/changeBudgetItem.js
@@ -1,15 +1,34 @@
 const buttons = document.querySelectorAll('.button-change-budget-item');
 
 for (let button of buttons) {
-    button.addEventListener('click', function () {
+    button.addEventListener('click', function (e) {
         let classNameForTableData = 'td.' + button.id;
         let chosenTableDatas = document.querySelectorAll(classNameForTableData);
         for (let td of chosenTableDatas) {
             td.contentEditable = 'true';
         }
-        button.innerHTML = 'Zmień';
+        button.hidden = true;
+        let buttonChangingBudgetItem = button.nextElementSibling.lastElementChild;
+        buttonChangingBudgetItem.style.display = 'inline-block';
+    })
+
+    button.parentElement.parentElement.addEventListener('mouseenter', function () {
+        button.innerHTML = '<i class="fas fa-pen ms-1 fs-6"></i>';
+    })
+
+    button.parentElement.parentElement.addEventListener('mouseleave', function () {
+        button.innerHTML = '';
     })
 }
+
+// const budgetItemInDetails = document.querySelectorAll('budget-item-details');
+// for (let item of budgetItemInDetails) {
+//     item.addEventListener('mouseenter', function () {
+//         button.style.display = 'block';
+//     })
+// }
+
+
 
 // var tds = document.getElementsByTagName('td');
 // for (var i = 0; i < tds.length; i++) {
