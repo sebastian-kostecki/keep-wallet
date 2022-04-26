294ce7e92a8332042ed094e188f5bdb7e47fe5ff
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-04-26 22:57:13 +0200

    Simplified Expenses model

--- App/Models/Expenses.php
+++ App/Models/Expenses.php
@@ -4,16 +4,9 @@ namespace App\Models;
 
 use PDO;
 
-class Expenses extends \Core\Model
+class Expenses extends BudgetItem
 {
-    public $errors = [];
-
-    public function __construct($data = [])
-    {
-        foreach ($data as $key => $value) {
-            $this->$key = $value;
-        }
-    }
+    protected const NAME_OF_TABLE_WITH_BUDGET_ITEMS = "expenses";
 
     public function save()
     {
@@ -39,38 +32,13 @@ class Expenses extends \Core\Model
 
     public function validate()
     {
-        if (!isset($this->amount)) {
-            $this->errors[] = 'Brak kwoty wydatku';
-        }
-
-        $this->amount = filter_var($this->amount, FILTER_VALIDATE_FLOAT);
-        if ((!$this->amount) || ($this->amount < 0) || (strlen(substr(strrchr($this->amount, "."), 1)) > 2)) {
-            $this->errors[] = 'Kwota jest nieprawidłowa';
-        }
-
-        if (!isset($this->date)) {
-            $this->errors[] = 'Nie wybrano daty wydatku';
-        }
-
-
-        $dateArr  = explode('/', $this->date);
-        if (count($dateArr) == 3) {
-            if (!(checkdate($dateArr[0], $dateArr[1], $dateArr[2]))) {
-                $this->errors[]  = 'Data jest nieprawidłowa';
-            }
-        }
-
-        if (!isset($this->paymentMethod)) {
-            $this->errors[] = 'Nie wybrano sposobu płatności';
-        }
-
-        if (!isset($this->category)) {
-            $this->errors[] = 'Nie wybrano kategorii wydatku';
-        }
-
-        if (strlen($this->comment) > 100) {
-            $this->errors[]  = 'Komentarz może zawierać maksymalnie 100 znaków';
-        }
+        $this->isAmountChosen();
+        $this->isAmountValid();
+        $this->isDateChosen();
+        $this->isDateValid();
+        $this->isPaymentMethodChosen();
+        $this->isCategoryChosen();
+        $this->isCommentValid();
     }
 
     public function change()
@@ -94,16 +62,5 @@ class Expenses extends \Core\Model
             return $query->execute();
         }
         return false;
-    }
-
-    public function remove()
-    {
-        $sql = "DELETE FROM expenses
-                WHERE id = :id";
-
-        $db = static::getDataBase();
-        $query = $db->prepare($sql);
-        $query->bindValue(':id', $this->id, PDO::PARAM_INT);
-        return $query->execute();
     }
 }
