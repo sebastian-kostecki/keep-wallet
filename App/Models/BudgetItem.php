a1b22b53606f0ae0a14b58dfb81b95f9071a0e14
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-04-26 22:28:01 +0200

    Added to BudgetItem model constructor and errors properties

--- App/Models/BudgetItem.php
+++ App/Models/BudgetItem.php
@@ -4,4 +4,12 @@ namespace App\Models;
 
 abstract class BudgetItem extends \Core\Model
 {
+    public $errors = [];
+
+    public function __construct($data = [])
+    {
+        foreach ($data as $key => $value) {
+            $this->$key = $value;
+        }
+    }
 }
