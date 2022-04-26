1ece9ab06b4cb5961d05ea4ae8c4fbf75f77dd0e
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-04-26 22:46:35 +0200

    Added function for validation in BudgetItem model

--- App/Models/BudgetItem.php
+++ App/Models/BudgetItem.php
@@ -26,4 +26,57 @@ abstract class BudgetItem extends \Core\Model
         $query->bindValue(':id', $this->id, PDO::PARAM_INT);
         return $query->execute();
     }
+
+    protected function isAmountChosen()
+    {
+        if (!isset($this->amount)) {
+            $this->errors[] = 'Brak kwoty';
+        }
+    }
+
+    protected function isAmountValid()
+    {
+        $this->amount = filter_var($this->amount, FILTER_VALIDATE_FLOAT);
+        if ((!$this->amount) || ($this->amount < 0) || (strlen(substr(strrchr($this->amount, "."), 1)) > 2)) {
+            $this->errors[] = 'Kwota jest nieprawidłowa';
+        }
+    }
+
+    protected function isDateChosen()
+    {
+        if (!isset($this->date)) {
+            $this->errors[] = 'Nie wybrano daty';
+        }
+    }
+
+    protected function isDateValid()
+    {
+        $dateArr  = explode('/', $this->date);
+        if (count($dateArr) == 3) {
+            if (!(checkdate($dateArr[0], $dateArr[1], $dateArr[2]))) {
+                $this->errors[]  = 'Data jest nieprawidłowa';
+            }
+        }
+    }
+
+    protected function isPaymentMethodChosen()
+    {
+        if (!isset($this->paymentMethod)) {
+            $this->errors[] = 'Nie wybrano sposobu płatności';
+        }
+    }
+
+    protected function isCategoryChosen()
+    {
+        if (!isset($this->category)) {
+            $this->errors[] = 'Nie wybrano kategorii';
+        }
+    }
+
+    protected function isCommentValid()
+    {
+        if (strlen($this->comment) > 100) {
+            $this->errors[]  = 'Komentarz może zawierać maksymalnie 100 znaków';
+        }
+    }
 }
