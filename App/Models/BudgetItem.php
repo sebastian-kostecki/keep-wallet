0fbeaa72170c16d11ba908dca76e85a3c5fb261c
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-04-26 22:35:49 +0200

    Added remove function to BudgetItem model

--- App/Models/BudgetItem.php
+++ App/Models/BudgetItem.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
+use PDO;
+
 abstract class BudgetItem extends \Core\Model
 {
+    protected const NAME_OF_TABLE_WITH_BUDGET_ITEMS = "";
     public $errors = [];
 
     public function __construct($data = [])
@@ -11,5 +14,16 @@ abstract class BudgetItem extends \Core\Model
         foreach ($data as $key => $value) {
             $this->$key = $value;
         }
+    }
+
+    public function remove()
+    {
+        $sql = "DELETE FROM " . self::NAME_OF_TABLE_WITH_BUDGET_ITEMS .
+            " WHERE id = :id";
+
+        $db = static::getDataBase();
+        $query = $db->prepare($sql);
+        $query->bindValue(':id', $this->id, PDO::PARAM_INT);
+        return $query->execute();
     }
 }
