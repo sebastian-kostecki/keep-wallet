77a8264c336ed7386cb84bdfb01d48e59e6ee241
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-04-26 23:00:25 +0200

    Simplified Incomes model

--- App/Models/Incomes.php
+++ App/Models/Incomes.php
@@ -4,17 +4,9 @@ namespace App\Models;
 
 use PDO;
 
-class Incomes extends \Core\Model
+class Incomes extends BudgetItem
 {
-    public $errors = [];
-    public $incomes = [];
-
-    public function __construct($data = [])
-    {
-        foreach ($data as $key => $value) {
-            $this->$key = $value;
-        }
-    }
+    protected const NAME_OF_TABLE_WITH_BUDGET_ITEMS = "incomes";
 
     public function save()
     {
@@ -38,25 +30,12 @@ class Incomes extends \Core\Model
 
     public function validate()
     {
-        $this->amount = filter_var($this->amount, FILTER_VALIDATE_FLOAT);
-        if ((!$this->amount) || ($this->amount < 0) || (strlen(substr(strrchr($this->amount, "."), 1)) > 2)) {
-            $this->errors[] = 'Kwota jest nieprawidłowa';
-        }
-
-        $dateArr  = explode('/', $this->date);
-        if (count($dateArr) == 3) {
-            if (!(checkdate($dateArr[0], $dateArr[1], $dateArr[2]))) {
-                $this->errors[]  = 'Data jest nieprawidłowa';
-            }
-        }
-
-        if (!isset($this->category)) {
-            $this->errors[] = 'Nie wybrano kategorii przychodu';
-        }
-
-        if (strlen($this->comment) > 100) {
-            $this->errors[]  = 'Komentarz może zawierać maksymalnie 100 znaków';
-        }
+        $this->isAmountChosen();
+        $this->isAmountValid();
+        $this->isDateChosen();
+        $this->isDateValid();
+        $this->isCategoryChosen();
+        $this->isCommentValid();
     }
 
     public function change()
@@ -78,16 +57,5 @@ class Incomes extends \Core\Model
             return $query->execute();
         }
         return false;
-    }
-
-    public function remove()
-    {
-        $sql = "DELETE FROM incomes
-                WHERE id = :id";
-
-        $db = static::getDataBase();
-        $query = $db->prepare($sql);
-        $query->bindValue(':id', $this->id, PDO::PARAM_INT);
-        return $query->execute();
     }
 }
