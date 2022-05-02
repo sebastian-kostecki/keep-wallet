bd666f4c12101116a450a0524b044288eda733a0
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-05-03 00:34:20 +0200

    Improved get current date - added up 1 into days

--- public/js/getCurrentMonth.js
+++ public/js/getCurrentMonth.js
@@ -3,10 +3,12 @@ const currentData = new Date();
 
 const getCurrentMonthPeriod = () => {
     const currentData = new Date();
-    currentData.setDate(1);
-    const firstDayCurrentMonth = currentData.toISOString().substr(0, 10);
-    currentData.setMonth(currentData.getMonth() + 1, 0);
-    const lastDayCurrentMonth = currentData.toISOString().substr(0, 10);
+    currentData.setDate(2);
+    const firstDayCurrentMonth = currentData.toISOString().slice(0, 10);
+    currentData.setMonth(currentData.getMonth() + 1, 1);
+    const lastDayCurrentMonth = currentData.toISOString().slice(0, 10);
+    console.log(firstDayCurrentMonth)
+    console.log(lastDayCurrentMonth)
     return firstDayCurrentMonth + "-" + lastDayCurrentMonth;
 }
 
