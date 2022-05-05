26acf19d9ce4368df7e975b3003b2610aaf9549a
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-05-06 00:00:02 +0200

    Added show limit for category

--- public/js/checkLimit.js
+++ public/js/checkLimit.js
@@ -9,6 +9,14 @@ for (let radioButton of selectCategoryButton) {
             .get(`/expense/getLimit/${categoryId}`)
             .then((data) => {
                 console.log(data.data);
+
+                if (data.data.limit_category) {
+                    const limitInfo = document.createElement('h1');
+                    const form = document.querySelector('.budget-form');
+                    form.prepend(limitInfo);
+
+                    limitInfo.textContent = data.data.limit_category;
+                }
             })
     })
 
