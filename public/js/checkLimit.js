362e68c8d6e2467fd7f7e1c1d37f959f6a1eab94
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-05-06 23:43:01 +0200

    Added show name category and limit

--- public/js/checkLimit.js
+++ public/js/checkLimit.js
@@ -1,7 +1,11 @@
+const showLimit = document.querySelector('#show-limit')
+const categoryName = document.querySelector('#category-name')
+const categoryLimit = document.querySelector('#category-limit')
+const categorySpent = document.querySelector('#category-spent')
+const categoryRemainded = document.querySelector('#category-remainded')
+
 const selectCategoryButton = document.querySelectorAll('.select-category-button')
 for (let radioButton of selectCategoryButton) {
-
-
     radioButton.addEventListener('change', function () {
         let categoryId = radioButton.value;
 
@@ -9,13 +13,14 @@ for (let radioButton of selectCategoryButton) {
             .get(`/expense/getLimit/${categoryId}`)
             .then((data) => {
                 console.log(data.data);
+                console.log(radioButton.id)
 
                 if (data.data.limit_category) {
-                    const limitInfo = document.createElement('h1');
-                    const form = document.querySelector('.budget-form');
-                    form.prepend(limitInfo);
-
-                    limitInfo.textContent = data.data.limit_category;
+                    showLimit.classList.remove('d-none')
+                    categoryName.textContent = radioButton.id;
+                    categoryLimit.textContent = data.data.limit_category;
+                } else if (!showLimit.classList.contains('d-none')) {
+                    showLimit.classList.add('d-none');
                 }
             })
     })
