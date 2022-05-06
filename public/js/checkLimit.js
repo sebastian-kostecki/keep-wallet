e066bb52fdeee035901a670b51cf75ac2850aec3
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-05-07 00:23:11 +0200

    Retrieved date for expenses

--- public/js/checkLimit.js
+++ public/js/checkLimit.js
@@ -1,3 +1,15 @@
+const getCurrentMonthPeriods = (date) => {
+    let day = parseInt(date.slice(9, 10));
+    let month = parseInt(date.slice(6, 7)) - 1;
+    let year = date.slice(0, 4);
+    let currentData = new Date(year, month, day);
+    currentData.setDate(2);
+    const firstDayCurrentMonth = currentData.toISOString().slice(0, 10);
+    currentData.setMonth(currentData.getMonth() + 1, 1);
+    const lastDayCurrentMonth = currentData.toISOString().slice(0, 10);
+    return firstDayCurrentMonth + "-" + lastDayCurrentMonth;
+}
+
 const showLimit = document.querySelector('#show-limit')
 const categoryName = document.querySelector('#category-name')
 const categoryLimit = document.querySelector('#category-limit')
@@ -12,13 +24,19 @@ for (let radioButton of selectCategoryButton) {
         axios
             .get(`/expense/getLimit/${categoryId}`)
             .then((data) => {
-                console.log(data.data);
-                console.log(radioButton.id)
-
                 if (data.data.limit_category) {
                     showLimit.classList.remove('d-none')
                     categoryName.textContent = radioButton.id;
                     categoryLimit.textContent = data.data.limit_category;
+
+                    //pobranie sumy wydatków podanej kategorii z określonego zakresu dat
+                    //pobieram początek i koniec na podstawie wybranej daty
+                    let date = document.querySelector('#date');
+                    let DateFormat = date.value;
+                    console.log(DateFormat);
+                    console.log(getCurrentMonthPeriods(DateFormat)) //to jest format pierwszy - ostatni
+
+
                 } else if (!showLimit.classList.contains('d-none')) {
                     showLimit.classList.add('d-none');
                 }
