4c62774d896fef2ca15d1e11be0fb92c4456be78
Sebastian Kostecki <sebastian.kostecki.programista@gmail.com>
2022-05-08 22:34:03 +0200

    Added refactured code

--- public/js/checkLimit.js
+++ public/js/checkLimit.js
@@ -1,124 +1,182 @@
-const getCurrentMonthPeriods = (date) => {
-    let day = parseInt(date.slice(9, 10));
-    let month = parseInt(date.slice(6, 7)) - 1;
-    let year = date.slice(0, 4);
-    let currentData = new Date(year, month, day);
-    currentData.setDate(2);
-    const firstDayCurrentMonth = currentData.toISOString().slice(0, 10);
-    currentData.setMonth(currentData.getMonth() + 1, 1);
-    const lastDayCurrentMonth = currentData.toISOString().slice(0, 10);
-    return firstDayCurrentMonth + "-" + lastDayCurrentMonth;
+const selectCategoryButton = document.querySelectorAll('.select-category-button')
+for (let radio of selectCategoryButton) {
+    radio.addEventListener('change', showLimit)
 }
 
-const isAmountValid = (value) => {
-    value *= 1000;
-    if (value % 10 == 0 && value > 0) {
-        return true;
+async function showLimit() {
+    let categoryId = this.value;
+    let limitCategory = await getLimit(categoryId);
+    if (limitCategory) {
+        let sumOfCategory = await getSumOfCategory(categoryId);
+        showLimitInformation(categoryId, limitCategory, sumOfCategory);
+        showDifferenceAfterTypingAmount(limitCategory, sumOfCategory);
+    } else {
+        hideWindowWithInformationsAboutLimit();
     }
+}
+
+const getLimit = async (categoryId) => {
+    let response = await axios.get(`/expense/getLimit/${categoryId}`);
+    return response.data.limit_category;
+}
+
+const getSumOfCategory = async (categoryId) => {
+    let selectedPeriod = getDateOfSelectedPeriod();
+    let response = await axios.get(`/expense/expenses/${categoryId}?date=${selectedPeriod}`);
+    return response.data.sum;
+}
+
+const getDateOfSelectedPeriod = () => {
+    let dateElement = document.querySelector('#date');
+    let dateRangeToDisplay = dateElement.value;
+    let dateRangeToDatabase = dateRangeToDisplay.convertDateToDatabaseFormat();
+    return dateRangeToDatabase;
+}
+
+String.prototype.convertDateToDatabaseFormat = function () {
+    let day = this.slice(8);
+    let month = this.slice(6, 7);
+    let year = this.slice(0, 4);
+    let date = new Date(year, month - 1, day);
+    date.setDate(2);
+    const firstDay = date.toISOString().slice(0, 10);
+    date.setMonth(date.getMonth() + 1, 1);
+    const lastDay = date.toISOString().slice(0, 10);
+    return firstDay + "-" + lastDay;
+}
+
+const showLimitInformation = (id, limit, sum) => {
+    showWindowWithInformationsAboutLimit();
+    changeWindowWithInformationAboutLimitColor(limit, sum);
+    assignName(id);
+    assignLimit(limit);
+    assignSum(sum);
+    assignDifference(limit, sum);
+}
+
+const showWindowWithInformationsAboutLimit = () => {
+    const windowWithInformationsAboutLimit = document.querySelector('#show-limit');
+    windowWithInformationsAboutLimit.classList.remove('d-none');
+}
+
+const assignName = (id) => {
+    const categoryName = document.querySelector('#category-name');
+    const selectedButton = document.querySelector(`.select-category-button[value="${id}"]`)
+    categoryName.textContent = selectedButton.id;
+}
+
+const assignLimit = (limit) => {
+    const categoryLimit = document.querySelector('#category-limit');
+    categoryLimit.textContent = limit;
+}
+
+const assignSum = (sum) => {
+    const categorySpent = document.querySelector('#category-spent');
+    categorySpent.textContent = sum;
+}
+
+const assignDifference = (limit, sum) => {
+    let difference = limit - sum;
+    if (difference >= 0) {
+        difference.assignRemaindedAmount();
+    } else {
+        difference.assignExceededAmount();
+    }
+}
+
+const changeWindowWithInformationAboutLimitColor = (limit, sum) => {
+    let difference = limit - sum;
+    if (difference >= 0) { changeColorToSuccess() }
+    else { changeColorToDanger() }
+}
+
+Number.prototype.assignRemaindedAmount = function () {
+    const categoryRemainded = document.querySelector('#category-remainded')
+    const headingRemainded = document.querySelector('#heading-remainded');
+    headingRemainded.textContent = "Pozostało: ";
+    categoryRemainded.textContent = this.toFixed(2);
+}
+
+Number.prototype.assignExceededAmount = function () {
+    const categoryRemainded = document.querySelector('#category-remainded')
+    const headingRemainded = document.querySelector('#heading-remainded');
+    headingRemainded.textContent = "Przekroczono: ";
+    categoryRemainded.textContent = Math.abs(this).toFixed(2);
+}
+
+const changeColorToSuccess = () => {
+    const windowWithInformationsAboutLimit = document.querySelector('#show-limit');
+    windowWithInformationsAboutLimit.classList.remove('bg-danger');
+    windowWithInformationsAboutLimit.classList.add('bg-success');
+}
+
+const changeColorToDanger = () => {
+    const windowWithInformationsAboutLimit = document.querySelector('#show-limit');
+    windowWithInformationsAboutLimit.classList.remove('bg-success');
+    windowWithInformationsAboutLimit.classList.add('bg-danger');
+}
+
+const showDifferenceAfterTypingAmount = (limit, sum) => {
+    const amountInput = document.querySelector('#amount');
+    showCurrentDifference(limit, sum);
+    amountInput.addEventListener('change', () => { showCurrentDifference(limit, sum) });
+}
+
+const showCurrentDifference = (limit, sum) => {
+    const amountInput = document.querySelector('#amount');
+    let amount = amountInput.value;
+    if (amount.isAmountCorrect()) {
+        let initialDifference = limit - sum;
+        let currentDifference = initialDifference - amount;
+        if (currentDifference >= 0) {
+            currentDifference.showRemaindedCurrentAmount();
+            changeColorToSuccess();
+        } else {
+            currentDifference.showExceededCurrentAmount();
+            changeColorToDanger();
+        }
+        showInformationAfterTypingAmount();
+    } else {
+        hideInformationAfterTypingAmount();
+    }
+}
+
+String.prototype.isAmountCorrect = function () {
+    if (this > 0 && this.isAmountCorrectFormat()) { return true }
+    else { return false }
+}
+
+String.prototype.isAmountCorrectFormat = function () {
+    let number = this * 1000;
+    if (number % 10 == 0) { return true }
     return false;
 }
 
-const showLimit = document.querySelector('#show-limit')
-const categoryName = document.querySelector('#category-name')
-const categoryLimit = document.querySelector('#category-limit')
-const categorySpent = document.querySelector('#category-spent')
-const categoryRemainded = document.querySelector('#category-remainded')
-const headingRemainded = document.querySelector('#heading-remainded');
-
-const showInfoAfterExpense = document.querySelector('#show-info-after-expense');
-const headingAfterExpense = document.querySelector('#info-after-expense-heading');
-const amountAfterExpense = document.querySelector('#info-after-expense-amount');
-
-const selectCategoryButton = document.querySelectorAll('.select-category-button')
-for (let radioButton of selectCategoryButton) {
-    radioButton.addEventListener('change', function () {
-        let categoryId = radioButton.value;
-
-        axios
-            .get(`/expense/getLimit/${categoryId}`)
-            .then((data) => {
-                if (data.data.limit_category) {
-                    showLimit.classList.remove('d-none')
-                    categoryName.textContent = radioButton.id;
-                    categoryLimit.textContent = data.data.limit_category;
-                    console.log(data.data.limit_category);
-
-                    //pobranie sumy wydatków podanej kategorii z określonego zakresu dat
-                    //pobieram początek i koniec na podstawie wybranej daty
-                    let dateElement = document.querySelector('#date');
-                    let DateFormat = dateElement.value;
-                    let date = getCurrentMonthPeriods(DateFormat);
-
-                    //wysyłamy żądanie zawierające id kategorii oraz zakres dat
-                    axios
-                        .get(`/expense/expenses/${categoryId}?date=${date}`)
-                        .then((d) => {
-                            console.log(d.data.sum);
-                            categorySpent.textContent = d.data.sum;
-
-                            //wpisujemy różnicę
-                            if (data.data.limit_category - d.data.sum > 0) {
-                                headingRemainded.textContent = "Pozostało: ";
-                                categoryRemainded.textContent = (data.data.limit_category - d.data.sum).toFixed(2);
-                                showLimit.classList.remove('bg-danger');
-                                showLimit.classList.add('bg-success');
-                            } else {
-                                headingRemainded.textContent = "Przekroczono: ";
-                                categoryRemainded.textContent = (d.data.sum - data.data.limit_category).toFixed(2);
-                                showLimit.classList.remove('bg-success');
-                                showLimit.classList.add('bg-danger');
-                            }
-
-                            //wyświetlamy albo nie podsumowanie w oparciu o kwotę
-                            //sytuacja, gdy najpierw wpiszemy kwotę
-                            const amountInput = document.querySelector('#amount');
-                            if (isAmountValid(amountInput.value)) {
-                                showInfoAfterExpense.classList.remove('d-none');
-
-                                //liczymy jaki będzie bilans pomiedzy tym co pozostało a nowym wydatkiem
-                                if ((data.data.limit_category - d.data.sum).toFixed(2) - amountInput.value > 0) {
-                                    headingAfterExpense.textContent = "Do limitu pozostanie ";
-                                    amountAfterExpense.textContent = (data.data.limit_category - d.data.sum).toFixed(2) - amountInput.value;
-                                    showLimit.classList.remove('bg-danger');
-                                    showLimit.classList.add('bg-success');
-                                } else {
-                                    headingAfterExpense.textContent = "Przekroczysz limit o ";
-                                    amountAfterExpense.textContent = amountInput.value - (data.data.limit_category - d.data.sum).toFixed(2);
-                                    showLimit.classList.remove('bg-success');
-                                    showLimit.classList.add('bg-danger');
-                                }
-
-                            } else {
-                                showInfoAfterExpense.classList.add('d-none');
-                            }
-
-                            //sytuacja, gdy najpierw wybierzemy kategorie a potem kwotę
-                            amountInput.addEventListener('change', function () {
-                                if (isAmountValid(amountInput.value)) {
-                                    showInfoAfterExpense.classList.remove('d-none');
-
-                                    if ((data.data.limit_category - d.data.sum).toFixed(2) - amountInput.value > 0) {
-                                        headingAfterExpense.textContent = "Do limitu pozostanie ";
-                                        amountAfterExpense.textContent = (data.data.limit_category - d.data.sum).toFixed(2) - amountInput.value;
-                                        showLimit.classList.remove('bg-danger');
-                                        showLimit.classList.add('bg-success');
-                                    } else {
-                                        headingAfterExpense.textContent = "Przekroczysz limit o ";
-                                        amountAfterExpense.textContent = amountInput.value - (data.data.limit_category - d.data.sum).toFixed(2);
-                                        showLimit.classList.remove('bg-success');
-                                        showLimit.classList.add('bg-danger');
-                                    }
-                                } else {
-                                    showInfoAfterExpense.classList.add('d-none');
-                                }
-                            })
-                        })
-
-                } else if (!showLimit.classList.contains('d-none')) {
-                    showLimit.classList.add('d-none');
-                }
-            })
-    })
-
+Number.prototype.showRemaindedCurrentAmount = function () {
+    const headingAfterExpense = document.querySelector('#info-after-expense-heading');
+    const amountAfterExpense = document.querySelector('#info-after-expense-amount');
+    headingAfterExpense.textContent = "Do limitu pozostanie ";
+    amountAfterExpense.textContent = this.toFixed(2);
 }
 
+Number.prototype.showExceededCurrentAmount = function () {
+    const headingAfterExpense = document.querySelector('#info-after-expense-heading');
+    const amountAfterExpense = document.querySelector('#info-after-expense-amount');
+    headingAfterExpense.textContent = "Przekroczysz limit o ";
+    amountAfterExpense.textContent = Math.abs(this).toFixed(2);
+}
+
+const showInformationAfterTypingAmount = () => {
+    const showInfoAfterExpense = document.querySelector('#show-info-after-expense');
+    showInfoAfterExpense.classList.remove('d-none');
+}
+
+const hideInformationAfterTypingAmount = () => {
+    const showInfoAfterExpense = document.querySelector('#show-info-after-expense');
+    showInfoAfterExpense.classList.add('d-none');
+}
+
+const hideWindowWithInformationsAboutLimit = () => {
+    const windowWithInformationsAboutLimit = document.querySelector('#show-limit');
+    windowWithInformationsAboutLimit.classList.add('d-none');
+}
